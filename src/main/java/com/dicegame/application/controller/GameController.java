f670c4584455ec47f138a688ad4e222f35cb80a3
Rafel <rasepe@gmail.com>
2020-02-22 20:39:43 +0100

    2202

--- src/main/java/com/dicegame/application/controller/GameController.java
+++ src/main/java/com/dicegame/application/controller/GameController.java
@@ -21,105 +21,105 @@ import javax.validation.constraints.NotNull;
 @RestController
 public class GameController {
 
-    @Autowired
-    private GameRepository gameRepository;
+	@Autowired
+	private GameRepository gameRepository;
 
-    @Autowired
-    private PlayerRepository playerRepository;
-
-	@CrossOrigin(origins = "http://localhost")
-    @GetMapping("/players/{playerId}/games")
-    public Page<Game> getAllGamesByPlayerId(@PathVariable (value = "playerId") Long playerId,
-                                                Pageable pageable) {
-        return gameRepository.findByPlayerId(playerId, null);  //pageable
-    }
+	@Autowired
+	private PlayerRepository playerRepository;
 
 	@CrossOrigin(origins = "http://localhost")
-    @PostMapping("/players/{playerId}/games")
-    public Game createGame(@PathVariable (value = "playerId") Long playerId, Game game)  { //@RequestBody Game game
-		
-        return playerRepository.findById(playerId).map(player -> {
-            game.setPlayer(player);
-          
-            int dice1 = (@NotNull int) Math.floor(Math.random()*6+1);
-            int dice2 = (@NotNull int) Math.floor(Math.random()*6+1);
-            game.setDice1(dice1);
-            game.setDice2(dice2);
-            
-            if (game.getDice1() + game.getDice2() == 7) {
-            	game.setHasWon(true); 
-        	}
-            
-            
-          
-           double points = 0;
-        	double successRate;
-        	List<Game> gamesByPlayer = new ArrayList<Game>();
-       	gamesByPlayer = gameRepository.findByPlayerId(player.getId(), null).getContent(); 
-    
-       	//suma els jocs anteriors
-       	for (int i=0; i<gamesByPlayer.size(); i++) {
-        		if(gamesByPlayer.get(i).isHasWon()) {
-        			points += 100;
-        		}
-       	}
-       	
-       	//i falta el joc actual
-   
-       if (game.isHasWon()) {
-    	   points += 100;
-       }
-       	
-       	successRate = points / (gamesByPlayer.size()+1);
-       	
-     // 	game.getPlayer().setSuccessRate(successRate);
-     	player.setSuccessRate(successRate);
-            
-           // if (game.getAuthor()=="") {
-          //  picture.setAuthor("ANONYMOUS");
-          //  };
-          //  shop.setNumPictures(shop.getNumPictures()+1);
-     	player.setHasGames(true); 
-     	
-            //return gameRepository.save(game);
-     	gameRepository.save(game);
-     	return game;
-        }).orElseThrow(() -> new ResourceNotFoundException("PlayerId " + playerId + " not found"));
-		
-		
-		}
-    
-
-//	@CrossOrigin(origins = "http://localhost")
-//    @PutMapping("/shops/{shopId}/pictures/{pictureId}")
-//    public Picture updatePicture(@PathVariable (value = "shopId") Long shopId,
-//                                 @PathVariable (value = "pictureId") Long pictureId,
-//                                 @Valid @RequestBody Picture pictureRequest) {
-//        if(!shopRepository.existsById(shopId)) {
-//            throw new ResourceNotFoundException("ShopId " + shopId + " not found");
-//        }
-//
-//        return pictureRepository.findById(pictureId).map(picture -> {
-//        	picture.setAuthor(pictureRequest.getAuthor());
-//            picture.setName(pictureRequest.getName());
-//            picture.setPrice(pictureRequest.getPrice());
-//            return pictureRepository.save(picture);
-//        }).orElseThrow(() -> new ResourceNotFoundException("PictureId " + pictureId + "not found"));
-//    }
-
-	
+	@GetMapping("/players/{playerId}/games")
+	public Page<Game> getAllGamesByPlayerId(@PathVariable (value = "playerId") Long playerId,
+			Pageable pageable) {
+		return gameRepository.findByPlayerId(playerId, null);  //pageable
+	}
 
 	@CrossOrigin(origins = "http://localhost")
-    @DeleteMapping("/players/{playerId}/games")
-    public void deleteAllGamesByPlayerId(@PathVariable (value = "playerId") Long playerId) {
-        
-	gameRepository.deleteAll(gameRepository.findByPlayerId(playerId, null));
-       //POSAR RATE A NULL:
-    Player player = playerRepository.getOne(playerId);
-    player.setSuccessRate(0);
-    playerRepository.save(player);
-    }
+	@PostMapping("/players/{playerId}/games")
+	public Game createGame(@PathVariable (value = "playerId") Long playerId, Game game)  { //@RequestBody Game game
 
-		
-	
+		return playerRepository.findById(playerId).map(player -> {
+			game.setPlayer(player);
+
+			int dice1 = (@NotNull int) Math.floor(Math.random()*6+1);
+			int dice2 = (@NotNull int) Math.floor(Math.random()*6+1);
+			game.setDice1(dice1);
+			game.setDice2(dice2);
+
+			if (game.getDice1() + game.getDice2() == 7) {
+				game.setHasWon(true); 
+			}
+
+
+
+			double points = 0;
+			double successRate;
+			List<Game> gamesByPlayer = new ArrayList<Game>();
+			gamesByPlayer = gameRepository.findByPlayerId(player.getId(), null).getContent(); 
+
+			//suma els jocs anteriors
+			for (int i=0; i<gamesByPlayer.size(); i++) {
+				if(gamesByPlayer.get(i).isHasWon()) {
+					points += 100;
+				}
+			}
+
+			//i falta el joc actual
+
+			if (game.isHasWon()) {
+				points += 100;
+			}
+
+			successRate = points / (gamesByPlayer.size()+1);
+
+			// 	game.getPlayer().setSuccessRate(successRate);
+			player.setSuccessRate(successRate);
+
+			// if (game.getAuthor()=="") {
+			//  picture.setAuthor("ANONYMOUS");
+			//  };
+			//  shop.setNumPictures(shop.getNumPictures()+1);
+			player.setHasGames(true); 
+
+			//return gameRepository.save(game);
+			gameRepository.save(game);
+			return game;
+		}).orElseThrow(() -> new ResourceNotFoundException("PlayerId " + playerId + " not found"));
+
+
+	}
+
+
+	//	@CrossOrigin(origins = "http://localhost")
+	//    @PutMapping("/shops/{shopId}/pictures/{pictureId}")
+	//    public Picture updatePicture(@PathVariable (value = "shopId") Long shopId,
+	//                                 @PathVariable (value = "pictureId") Long pictureId,
+	//                                 @Valid @RequestBody Picture pictureRequest) {
+	//        if(!shopRepository.existsById(shopId)) {
+	//            throw new ResourceNotFoundException("ShopId " + shopId + " not found");
+	//        }
+	//
+	//        return pictureRepository.findById(pictureId).map(picture -> {
+	//        	picture.setAuthor(pictureRequest.getAuthor());
+	//            picture.setName(pictureRequest.getName());
+	//            picture.setPrice(pictureRequest.getPrice());
+	//            return pictureRepository.save(picture);
+	//        }).orElseThrow(() -> new ResourceNotFoundException("PictureId " + pictureId + "not found"));
+	//    }
+
+
+
+	@CrossOrigin(origins = "http://localhost")
+	@DeleteMapping("/players/{playerId}/games")
+	public void deleteAllGamesByPlayerId(@PathVariable (value = "playerId") Long playerId) {
+
+		gameRepository.deleteAll(gameRepository.findByPlayerId(playerId, null));
+		//POSAR RATE A NULL:
+		Player player = playerRepository.getOne(playerId);
+		player.setSuccessRate(0);
+		playerRepository.save(player);
+	}
+
+
+
 }
--- src/main/java/com/dicegame/application/controller/PlayerController.java
+++ src/main/java/com/dicegame/application/controller/PlayerController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.validation.Valid;
 
@@ -23,192 +24,192 @@ import javax.validation.Valid;
 @CrossOrigin(origins = "http://localhost")
 public class PlayerController {
 
-    @Autowired
-    private PlayerRepository playerRepository;
-    
-    @Autowired
-    private GameRepository gameRepository;
+	@Autowired
+	private PlayerRepository playerRepository;
 
-	
-    @GetMapping("/players")
-    public Page<Player> getAllPlayers(Pageable pageable) {
-        return playerRepository.findAll(pageable);
-    }
-    
-    
-    @GetMapping("/players/ranking")
-    public double getAllPlayersRanking(Pageable pageable) {
-       // return playerRepository.findAll(pageable);
-        
-       // playerRepository.findAll(pageable).getNumberOfElements();
-       // playerRepository.findAll(pageable).getContent();
-        
-        double addition = 0;
-        
-        for (int i=0; i<gameRepository.findAll(pageable).getNumberOfElements(); i++) {
-        	if (gameRepository.findAll(pageable).getContent().get(i).isHasWon()) {
-        		addition +=100;
-        	}
-        	
-        }
-        
-        
-        double average = addition / gameRepository.findAll(pageable).getNumberOfElements();
-        
-        return average;
-        
-    }
-	
-    
-    
-    @GetMapping("/players/ranking/loser")
-    public ArrayList<Player> getLoserRanking(Pageable pageable) {
-       // return playerRepository.findAll(pageable);
-        
-        playerRepository.findAll(pageable).getNumberOfElements();
-        playerRepository.findAll(pageable).getContent();
-        
-        double playeraverage = 0;
-        double minimum = 1000;
-       // long loserId;
-        Player loser = null;
-        ArrayList<Player> losers = new ArrayList<Player>(); 
-        for (int i=0; i<playerRepository.findAll(pageable).getNumberOfElements(); i++) {
-//        	if (gameRepository.findAll(pageable).getContent().get(i).isHasWon()) {
-//        		addition +=100;
-//        	}
-        	
-        	if (playerRepository.findAll(pageable).getContent().get(i).isHasGames()) {
-        		playeraverage = playerRepository.findAll(pageable).getContent().get(i).getSuccessRate();
-        		if (minimum > playeraverage) {
-            		minimum=playeraverage;
-            		//loserId = playerRepository.findAll(pageable).getContent().get(i).getId();
-            		loser = playerRepository.findAll(pageable).getContent().get(i);
-            		if(losers.size() > 0 ) {
-           			 losers.remove( losers.size() - 1 );
-           		}           
-           		losers.add(loser);
-            		} else if (minimum==playeraverage) {
-            			minimum=playeraverage;
-                		//loserId = playerRepository.findAll(pageable).getContent().get(i).getId();
-                		loser = playerRepository.findAll(pageable).getContent().get(i);
-                		losers.add(loser);
-            		}
-        		
-        	}
-        		
-        	
-        }
-        
-        
-       // double average = addition / gameRepository.findAll(pageable).getNumberOfElements();
-        
-        return losers;
-        
-    }
-    
-    
-    
-    @GetMapping("/players/ranking/winner")
-    public ArrayList<Player> getWinnerRanking(Pageable pageable) {
-       // return playerRepository.findAll(pageable);
-        
-        playerRepository.findAll(pageable).getNumberOfElements();
-        playerRepository.findAll(pageable).getContent();
-        
-        double playeraverage = 0;
-        double maximum = 0;
-       // long loserId;
-        Player winner = null;
-        ArrayList<Player> winners = new ArrayList<Player>(); 
-        
-        for (int i=0; i<playerRepository.findAll(pageable).getNumberOfElements(); i++) {
-//        	if (gameRepository.findAll(pageable).getContent().get(i).isHasWon()) {
-//        		addition +=100;
-//        	}
-        	
-        	if (playerRepository.findAll(pageable).getContent().get(i).isHasGames()) {
-        		playeraverage = playerRepository.findAll(pageable).getContent().get(i).getSuccessRate();
-        		if (maximum < playeraverage) {
-            		maximum=playeraverage;
-            		//loserId = playerRepository.findAll(pageable).getContent().get(i).getId();
-            		winner = playerRepository.findAll(pageable).getContent().get(i);
-            		if(winners.size() > 0 ) {
-            			 winners.remove( winners.size() - 1 );
-            		}           
-            		winners.add(winner);
-            		} else if (maximum == playeraverage) {
-            			maximum=playeraverage;
-                		//loserId = playerRepository.findAll(pageable).getContent().get(i).getId();
-                		winner = playerRepository.findAll(pageable).getContent().get(i);
-                		winners.add(winner);
-            		}
-        		
-        	}
-        		
-        	
-        }
-        
-        
-       // double average = addition / gameRepository.findAll(pageable).getNumberOfElements();
-        
-        return winners;
-        
-    }
-	
-//	// Single item
-//	@GetMapping("/shops/{shopId}")
-//	Shop one(@PathVariable Long shopId) {
-//		
-//		return shopRepository.findById(shopId)
-//			.orElseThrow(() -> new ResourceNotFoundException("ShopId " + shopId + " not found"));
-//	}
-//	
-	
-    @PostMapping("/players")
-    public Player createPlayer(@RequestBody Player player) {
-    	  
-             // player.setName("ANONYMOUS");
-              //}; }
-    	player.setCreatedAt(LocalDateTime.now());
-    	  if (player.getName()=="") {
-              player.setName(null);
-              };
-       // return playerRepository.save(player);
-              playerRepository.save(player);
-              return playerRepository.findAll().get(playerRepository.findAll().size()-1);
-               
-    }
+	@Autowired
+	private GameRepository gameRepository;
 
-//    @CrossOrigin(origins = "http://localhost")
-//    @PutMapping("/players/{playerId}")
-//   public Player updatePlayer(@PathVariable Long playerId, Player playerRequest) {  //@Valid @RequestBody
-//        return playerRepository.findById(playerId).map(player -> {
-//           player.setName(playerRequest.getName());
-//            return playerRepository.save(player);
-//        }).orElseThrow(() -> new ResourceNotFoundException("PlayerId " + playerId + " not found"));
-//  }
-    
-	
-	  @CrossOrigin(origins = "http://localhost")
-	  @PutMapping("/players/{id}") Player replacePlayer(@RequestBody Player
-	  newPlayer, @PathVariable Long id) {
-	  
-	  return playerRepository.findById(id) .map(player -> {
-	  player.setName(newPlayer.getName()); return playerRepository.save(player); })
-	  .orElseGet(() -> { newPlayer.setId(id); return
-	  playerRepository.save(newPlayer); }); }
-	 
-    
-    
 
-//
-//    @DeleteMapping("/shops/{shopId}")
-//    public ResponseEntity<?> deleteShop(@PathVariable Long shopId) {
-//        return shopRepository.findById(shopId).map(shop -> {
-//            shopRepository.delete(shop);
-//            return ResponseEntity.ok().build();
-//        }).orElseThrow(() -> new ResourceNotFoundException("ShopId " + shopId + " not found"));
-//    }
+	@GetMapping("/players")
+	public List<Player> getAllPlayers() { //Page<Player> getAllPlayers(Pageable pageable)
+		return playerRepository.findAll();
+	}
+
+
+	@GetMapping("/players/ranking")
+	public double getAllPlayersRanking() {
+		// return playerRepository.findAll(pageable);
+
+		// playerRepository.findAll(pageable).getNumberOfElements();
+		// playerRepository.findAll(pageable).getContent();
+
+		double addition = 0;
+
+		for (int i=0; i<gameRepository.findAll().size(); i++) {
+			if (gameRepository.findAll().get(i).isHasWon()) {
+				addition +=100;
+			}
+
+		}
+
+
+		double average = addition / gameRepository.findAll().size();
+
+		return average;
+
+	}
+
+
+
+	@GetMapping("/players/ranking/loser")
+	public ArrayList<Player> getLoserRanking() {
+		// return playerRepository.findAll(pageable);
+
+		playerRepository.findAll().size();
+		playerRepository.findAll();
+
+		double playeraverage = 0;
+		double minimum = 1000;
+		// long loserId;
+		Player loser = null;
+		ArrayList<Player> losers = new ArrayList<Player>(); 
+		for (int i=0; i<playerRepository.findAll().size(); i++) {
+			//        	if (gameRepository.findAll(pageable).getContent().get(i).isHasWon()) {
+			//        		addition +=100;
+			//        	}
+
+			if (playerRepository.findAll().get(i).isHasGames()) {
+				playeraverage = playerRepository.findAll().get(i).getSuccessRate();
+				if (minimum > playeraverage) {
+					minimum=playeraverage;
+					//loserId = playerRepository.findAll(pageable).getContent().get(i).getId();
+					loser = playerRepository.findAll().get(i);
+					if(losers.size() > 0 ) {
+						losers.remove( losers.size() - 1 );
+					}           
+					losers.add(loser);
+				} else if (minimum==playeraverage) {
+					minimum=playeraverage;
+					//loserId = playerRepository.findAll(pageable).getContent().get(i).getId();
+					loser = playerRepository.findAll().get(i);
+					losers.add(loser);
+				}
+
+			}
+
+
+		}
+
+
+		// double average = addition / gameRepository.findAll(pageable).getNumberOfElements();
+
+		return losers;
+
+	}
+
+
+
+	@GetMapping("/players/ranking/winner")
+	public ArrayList<Player> getWinnerRanking() {
+		// return playerRepository.findAll(pageable);
+
+		playerRepository.findAll().size();
+		playerRepository.findAll();
+
+		double playeraverage = 0;
+		double maximum = 0;
+		// long loserId;
+		Player winner = null;
+		ArrayList<Player> winners = new ArrayList<Player>(); 
+
+		for (int i=0; i<playerRepository.findAll().size(); i++) {
+			//        	if (gameRepository.findAll(pageable).getContent().get(i).isHasWon()) {
+			//        		addition +=100;
+			//        	}
+
+			if (playerRepository.findAll().get(i).isHasGames()) {
+				playeraverage = playerRepository.findAll().get(i).getSuccessRate();
+				if (maximum < playeraverage) {
+					maximum=playeraverage;
+					//loserId = playerRepository.findAll(pageable).getContent().get(i).getId();
+					winner = playerRepository.findAll().get(i);
+					if(winners.size() > 0 ) {
+						winners.remove( winners.size() - 1 );
+					}           
+					winners.add(winner);
+				} else if (maximum == playeraverage) {
+					maximum=playeraverage;
+					//loserId = playerRepository.findAll(pageable).getContent().get(i).getId();
+					winner = playerRepository.findAll().get(i);
+					winners.add(winner);
+				}
+
+			}
+
+
+		}
+
+
+		// double average = addition / gameRepository.findAll(pageable).getNumberOfElements();
+
+		return winners;
+
+	}
+
+	//	// Single item
+	//	@GetMapping("/shops/{shopId}")
+	//	Shop one(@PathVariable Long shopId) {
+	//		
+	//		return shopRepository.findById(shopId)
+	//			.orElseThrow(() -> new ResourceNotFoundException("ShopId " + shopId + " not found"));
+	//	}
+	//	
+
+	@PostMapping("/players")
+	public Player createPlayer(@RequestBody Player player) {
+
+		// player.setName("ANONYMOUS");
+		//}; }
+		player.setCreatedAt(LocalDateTime.now());
+		if (player.getName()=="") {
+			player.setName(null);
+		};
+		// return playerRepository.save(player);
+		playerRepository.save(player);
+		return playerRepository.findAll().get(playerRepository.findAll().size()-1);
+
+	}
+
+	//    @CrossOrigin(origins = "http://localhost")
+	//    @PutMapping("/players/{playerId}")
+	//   public Player updatePlayer(@PathVariable Long playerId, Player playerRequest) {  //@Valid @RequestBody
+	//        return playerRepository.findById(playerId).map(player -> {
+	//           player.setName(playerRequest.getName());
+	//            return playerRepository.save(player);
+	//        }).orElseThrow(() -> new ResourceNotFoundException("PlayerId " + playerId + " not found"));
+	//  }
+
+
+	@CrossOrigin(origins = "http://localhost")
+	@PutMapping("/players/{id}") Player replacePlayer(@RequestBody Player
+			newPlayer, @PathVariable Long id) {
+
+		return playerRepository.findById(id) .map(player -> {
+			player.setName(newPlayer.getName()); return playerRepository.save(player); })
+				.orElseGet(() -> { newPlayer.setId(id); return
+						playerRepository.save(newPlayer); }); }
+
+
+
+
+	//
+	//    @DeleteMapping("/shops/{shopId}")
+	//    public ResponseEntity<?> deleteShop(@PathVariable Long shopId) {
+	//        return shopRepository.findById(shopId).map(shop -> {
+	//            shopRepository.delete(shop);
+	//            return ResponseEntity.ok().build();
+	//        }).orElseThrow(() -> new ResourceNotFoundException("ShopId " + shopId + " not found"));
+	//    }
 
 }
